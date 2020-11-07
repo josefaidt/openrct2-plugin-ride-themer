45116e1a99bd6af27bcae411cf44154f4c2de33f
josef <josef.aidt@gmail.com>
2020-11-07 14:09:15 -0600

    add version to park message

--- action.js
+++ action.js
@@ -1,8 +1,10 @@
+import { version } from './package.json'
+
 export default function action() {
   try {
     park.postMessage({
       type: 'award',
-      text: 'ride-themer.js has been initialised!',
+      text: `ride-themer ${version} has been initialised!`,
     })
   } catch (error) {
     console.log(error)
